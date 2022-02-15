0b8e9981644a35cae8521550c0a0300b93affb74
xander.storme <xander.storme@student.howest.be>
2022-02-15 20:14:45 +0100

    Add Loan Entity

--- infrastructure/src/main/java/org/tinylibrary/entity/Loan.java
+++ infrastructure/src/main/java/org/tinylibrary/entity/Loan.java
@@ -1,4 +1,31 @@
 package org.tinylibrary.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate lendingDate;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "lender_id")
+    private Patron lender;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
